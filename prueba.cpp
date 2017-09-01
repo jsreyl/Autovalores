7a14ae648b3300640d018d44ae26a6df3c3ea53e
Johan <jsriosg@unal.edu.co>
2017-09-01 11:07:59 -0500

    version final prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -28,14 +28,12 @@ int main(){
   }
   SelfAdjointEigenSolver<MatrixXd> es(s);
   std::cout<<"\n"<<"i"<<"\t"<<"Auto valor"<<"\t"<<"Auto valor t"<<"\t"<<"error porcentual \n"<<std::endl;
-  //std::cout.setf(std::ios::scientific);
   for(int k=1;k<=n;k++){
     double E = es.eigenvalues()[n-k];
     double E_r = (1)/(2-(2*std::cos(((2*k)-1)*(M_PI)/((2*n)+1))));  
     double d = (std::abs(E-E_r))/E_r;
     std::cout<<k<<"\t"<<E<<"\t"<<E_r<<"\t"<<d<<std::endl;
   }
-  //std::cout.unsetf(std::ios::scientific);
   std::cout<<"\n"<<"Matriz de auto vectores \n\n";
   std::cout<<es.eigenvectors()<<std::endl;
 }
